96fbdfd1a22884054b13f1c289e0765f9dab74f7
victor-gaivenis <129993733+victor-gaivenis@users.noreply.github.com>
2023-04-11 23:01:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
+from statsmodels. stats.weightstats import ztest as ztest
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 987333364 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +11,9 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.04
+    count = np.array([x_success, y_success])
+    nobs = np.array([x_cnt, y_cnt]) 
+    zstat, pvalue = proposotions_ztest(count = count, nobs = nobs, alternative='larger')
+    return pvalue < alpha
+    return pvalue < alpha # Ваш ответ, True или False
